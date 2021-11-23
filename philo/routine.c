19f9b2cec775d19d4d7e39dfc3ccf1ef2afe0635
Florian CATINAUD <fcatinau@e3r13p6.42.fr>
2021-11-23 15:29:28 +0100

    WIP He got fork now check if work

--- philo/routine.c
+++ philo/routine.c
@@ -6,7 +6,7 @@
 /*   By: fcatinau <fcatinau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 15:52:20 by fcatinau          #+#    #+#             */
-/*   Updated: 2021/11/22 20:00:25 by fcatinau         ###   ########.fr       */
+/*   Updated: 2021/11/23 15:27:38 by fcatinau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,53 +35,9 @@ void	change_state(t_philo *philo, t_status new_state)
 	pthread_mutex_unlock(philo->mutex_alive);
 }
 
-static void	get_fork(t_philo *philo, int status)
-{
-	int	i;
-	int	pos;
-
-	pos = 0;
-	if (status)
-	{
-		pthread_mutex_unlock(philo->fork_left);
-		pthread_mutex_unlock(philo->fork_right);
-		philo->fork_left = NULL;
-		philo->fork_right = NULL;
-		while (pos < 2)
-		{
-			philo->use[pos - 1] = 0;
-			pos++;
-		}
-	}
-	else
-	{
-		while (!philo->fork_left && !philo->fork_right)
-		{
-			i = 0;
-			while (i < philo->param[NB_PHILO])
-			{
-				if (philo->fork_use[i] == 0)
-				{
-					if (!philo->fork_left)
-						philo->fork_left = &philo->forks[i];
-					else if (!philo->fork_right)
-						philo->fork_right = &philo->forks[i];
-					philo->fork_use[i] = 1;
-					philo->use[pos] = i + 1;
-					pos++;
-				}
-				i++;
-			}
-		}
-
-	}
-	return ;
-}
-
 static void	routine_eat(t_philo *philo)
 {
 	// looking for fork ?
-	get_fork(philo, 0);// work
 	pthread_mutex_lock(philo->fork_left);// !!! can't lock a fork --> need to find a other way
 	pthread_mutex_lock(philo->fork_right);
 	pthread_mutex_lock(philo->mutex_alive);
@@ -92,10 +48,6 @@ static void	routine_eat(t_philo *philo)
 	philo->time_prev_eat = get_time();
 	philo->already_eat++;
 	ft_sleep(philo->param[TIME_TO_EAT]);
-	dprintf(2, "Enter in get_fork 1\n");
-	// get bye fork
-	get_fork(philo, 1);// dont work
-	dprintf(2, "get out\n");
 }
 
 static void	routine_sleep(t_philo *philo)
